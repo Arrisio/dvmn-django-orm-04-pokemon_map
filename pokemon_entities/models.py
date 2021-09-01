487e43e006d811f578d3989e24b8624eb779c645
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-01 13:48:38 +0300

    feat: add verbose names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,18 +2,18 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.TextField()
-    photo = models.ImageField(upload_to="pokemons", null=True, blank=True)
+    title = models.TextField(verbose_name='имя')
+    photo = models.ImageField(verbose_name='картинка', upload_to="pokemons", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='покемон', on_delete=models.CASCADE)
 
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField(verbose_name='широта')
+    lon = models.FloatField(verbose_name='долгота')
 
     appeared_at = models.DateTimeField(verbose_name='появится', null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name='исчезнет', null=True, blank=True)
