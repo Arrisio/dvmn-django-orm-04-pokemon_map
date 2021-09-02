cbb1bf4f84fef1cfd6b7f95af351f48d522135ae
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-02 11:43:56 +0300

    fix: iss-1-5 remove useless comments

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,5 +1,4 @@
 import folium
-import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
@@ -28,16 +27,10 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    # with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-    #     pokemons = json.load(database)["pokemons"]
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in PokemonEntity.objects.all():
         add_pokemon(
-            folium_map,
-            pokemon.lat,
-            pokemon.lon,
-            pokemon.pokemon.photo.path
+            folium_map, pokemon.lat, pokemon.lon, pokemon.pokemon.photo.path
         )
 
     pokemons_on_page = []
