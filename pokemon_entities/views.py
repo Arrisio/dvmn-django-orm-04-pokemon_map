94b012a8c997cd5006f61bf559c7f998b98a1abf
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-02 12:00:21 +0300

    perf: use prefetch related entities

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -55,7 +55,7 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
+        pokemon = Pokemon.objects.prefetch_related('entities').get(id=pokemon_id)
     except Pokemon.DoesNotExist:
         return HttpResponseNotFound("<h1>Такой покемон не найден</h1>")
 
